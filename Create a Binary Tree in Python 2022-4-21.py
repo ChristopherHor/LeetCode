2c46eadf4cceb7efc19c5c0bbe429b7501416a41
ChristopherHor <43395701+ChristopherHor@users.noreply.github.com>
2022-04-24 00:44:26 +0900

    Cleaned comments

--- Create a Binary Tree in Python 2022-4-21.py
+++ Create a Binary Tree in Python 2022-4-21.py
@@ -6,23 +6,20 @@
 #     8 9 
 # Includes creating nodes and printing only.  Excludes .add() , .insert(), .delete(), etc.
 
-
-#            1
-#         2     3
-#       4  5
-
-
 class Node:
+    
     def __init__(self, value = None, nodeParent = None):
         self.left = None
         self.right = None
         self.value = value
         self.parent = nodeParent
+        
     def checkChildFull(self, node):
         childIsFull = False
         if node is not None:
             if node.left is not None and node.right is not None:
                 return True   
+            
     def findEmptyChild(self, node):
         # Returns the first empty node (node is None)
         if node is None:
@@ -40,16 +37,7 @@ class Node:
                 return self.findEmptyChild(node.parent.right)
             else:
                 return self.findEmptyChild(node.left)    
-        #return self.findEmptyChild(node.left, switchLeftRight)
-        #return self.findEmptyChild(node.right, switchLeftRight)
-        #return self.findEmptyChild(node.parent.right, switchLeftRight)
-        #if switchLeftRight:
-        #    #switchLeftRight = 1 - switchLeftRight #flip between 0 (left) and 1 (right)
-        #    return self.findEmptyChild(node.right, switchLeftRight)
-        #    #return self.findEmptyChild(node.parent, switchLeftRight)
-        #else:
-        #    switchLeftRight = 1 - switchLeftRight #flip between 0 (left) and 1 (right)
-        #    return self.findEmptyChild(node.left, switchLeftRight)
+        
     def insertValue(self, node, value):
         # Does not make balanced trees but makes somewhat balanced branches.
         successFlag = False
@@ -69,26 +57,8 @@ class Node:
             emptyNode = Node(value, emptyNodeParent)
             successFlag = True
             return successFlag
-            #if node.left is None:
-            #    node.left = Node(value, node)
-            #    successFlag = True
-            #    return successFlag
-            #if node.right is None:
-            #    node.right = Node(value, node)
-            #    successFlag = True
-            #    return successFlag
-            #
-            #if self.checkChildFull(node.left):
-            #    if self.checkChildFull(node.right):
-            #        # if both left and right children are full,
-            #        # go ahead and try to insert to the left side
-            #        # and let the recursions eventually find an opening
-            #        self.insertValue(node.left, value)
-            #    else: # right child node is not full, so try to insert there
-            #        self.insertValue(node.right, value)
-            #else: # left child node is not full, so try to insert there
-            #    self.insertValue(node.left, value)
         return successFlag
+    
     def printTree(self, node, level=0):
         # Prints a rotated binary tree, so that the root is on the left and the leaves on the right.
         # "Left" nodes visually appear below while "right" nodes visually appear above
@@ -129,15 +99,3 @@ n1.printTree(n1)
              9
          4
              8
-
-            ## inserNode....., level = 0, parentNode = None, switchLeftRight = 0
-                ##if switchLeftRight == 0:
-                ##    switchLeftRight = 1 - switchLeftRight #swaps between 0 and 1; 0 is left, 1 is right
-                ##    self.insertNode(node.left, value, level+1, node, switchLeftRight)
-                ##else:
-                ##    switchLeftRight = 1 - switchLeftRight #swaps between 0 and 1; 0 is left, 1 is right
-                ##    self.insertNode(node.right, value, level+1, node, switchLeftRight) #Only add a level deeper if for the right node
-                ###go into Node2, Level=1, parent is 1, switch=1
-                
-            
-            
